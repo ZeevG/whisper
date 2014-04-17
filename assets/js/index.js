1700d0ba3ecdd5009a86978b03b10ffec4037366
Ze'ev Gilovitz <zeev.gil@gmail.com>
2014-04-17 22:54:11 +0800

    Highlight.js initialise function

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,6 +7,8 @@
     "use strict";
 
     $(document).ready(function(){
+        
+        $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
 
         $(".post-content").fitVids();
         $(".site-head").parallax("50%", .7);
